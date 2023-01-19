d5c4f5e8d6f83820e3714f814c0e2749f8ce4059
sabicera <117107103+sabicera@users.noreply.github.com>
2023-01-20 01:10:53 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,70 +1,252 @@
-namespace SMS
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        private List<DateTime> cyprusHolidays = new List<DateTime>();
+
+        string text1 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н÷ н∙н⌡н∙н⌠н╖н÷нё н╓н÷н╔ н═н║н÷н≥н÷н²н╓н÷нё н÷н⌡н÷н н⌡н≈н║н╘н≤н≈н н∙. н°н═н÷н║н∙н≥н╓н∙ н²н▒ н═н∙н║н▒нён∙н╓н∙ н²н▒ н╓н÷ н═н▒н║н▒н⌡н▒н▓н∙н╓н∙.";
+        string text2 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н≈ н∙н═н≥нён н∙н╔н≈ н╓н÷н╔ н═н║н÷н≥н÷н²н╓н÷нё н÷н⌡н÷н н⌡н≈н║н╘н≤н≈н н∙. н°н═н÷н║н∙н≥н╓н∙ н²н▒ н═н∙н║н▒нён∙н╓н∙ н²н▒ н╓н÷ н═н▒н║н▒н⌡н▒н▓н∙н╓н∙.";
+        string text3 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н≈ н▒н²н╓н≥н н▒н╓н▒нён╓н▒нён≈ н╓н÷н╔ н═н║н÷н≥н÷н²н╓н÷нё н÷н⌡н÷н н⌡н≈н║н╘н≤н≈н н∙. н°н═н÷н║н∙н≥н╓н∙ н²н▒ н═н∙н║н▒нён∙н╓н∙ н²н▒ н╓н÷ н═н▒н║н▒н⌡н▒н▓н∙н╓н∙.";
+        string text4 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н═н▒н║н▒н н▒н⌡н╘ н∙н═н≥н н÷н≥н²н╘н²н≈нён╓н∙ н▒н°н∙нён▒ н°н▒н√н≥ н°н▒нё.";
+        string text5 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н╓н÷ н═н║н÷н≥н÷н² нён▒нё н∙н≥н²н▒н≥ н═н║н÷н▓н⌡HMAн╓н≥н н÷. н∙н╖н∙н╓н∙  н═н≥нён╓н╘нён≈ н∙н╔н║н╘. н⌠н≥н▒ н²н▒ н═н▒н║н∙н╓н∙ н н▒н╓н≥ н▒н⌡н⌡н÷.";
+        string text6 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н∙н╖н∙н╓н∙ н═н≥нён╓н╘нён≈  н∙н╔н║н╘. н⌠н≥н▒ н²н▒ н═н▒н║н∙н╓н∙ н н▒н╓н≥ н▒н⌡н⌡н÷.";
+        string text7 = "н н▒н╓н▒нён╓н≈н°н▒ н⌡н∙н°н∙нён÷н╔. н╓н∙н╖н²н≥н н÷ н╓н°н≈н°н▒. н╓н÷ н═н║н÷н≥н÷н² нён▒нё н═н║н÷н╘н≤н∙н≥н╓н▒н≥ нён╓н≈н² н▒н²н╓н≥н═н║н÷нён╘н═н∙н≥н▒ н⌠н≥н▒ н∙н⌡н∙н⌠н╖н÷.";
+
+        private void AddHolidays(int year)
+        {
+            //нёо└н╠н╦н╣о│н╜о┌ н∙н©о│о└н╜о┌
+            cyprusHolidays.Add(new DateTime(year, 1, 1)); // н═о│о┴о└н©о┤о│н©н╫н╞н╠
+            cyprusHolidays.Add(new DateTime(year, 1, 6)); // н≤н╣н©о├н╛н╫н╣н╧н╠
+            cyprusHolidays.Add(new DateTime(year, 3, 25)); // 25н╥ н°н╠о│о└н╞н©о┘
+            cyprusHolidays.Add(new DateTime(year, 4, 1)); // н▒н╫н╣н╬н╠о│о└н╥о┐н╞н╠ о└н╥о┌ н о█о─о│н©о┘
+            cyprusHolidays.Add(new DateTime(year, 5, 1)); // н∙о│нЁн╠о└н╧н╨н╝ н═о│о┴о└н©н╪н╠о┼н╠
+            cyprusHolidays.Add(new DateTime(year, 8, 15)); // н н©н╞н╪н╥о┐н╥ о└н╥о┌ н≤н╣н©о└о▄н╨н©о┘
+            cyprusHolidays.Add(new DateTime(year, 10, 1)); // н≈н╪н╜о│н╠ н▒н╫н╣н╬н╠о│о└н╥о┐н╞н╠о┌ о└н╥о┌ н о█о─о│н©о┘
+            cyprusHolidays.Add(new DateTime(year, 10, 28)); // н≈н╪н╜о│н╠ о└н©о┘ н▄о┤н╧
+            cyprusHolidays.Add(new DateTime(year, 12, 25)); // н╖о│н╧о┐о└н©о█нЁн╣н╫н╫н╠
+
+            //н н╧н╫н╥о└н╜о┌ н∙н©о│о└н╜о┌
+            cyprusHolidays.Add(new DateTime(2022, 12, 26)); // н╖о│н╧о┐о└н©о█нЁн╣н╫н╫н╠ 2023
+            cyprusHolidays.Add(new DateTime(2023, 1, 2)); // н═о│о┴о└н©о┤о│н©н╫н╞н╠ 2023
+            cyprusHolidays.Add(new DateTime(2023, 2, 27)); // н н╠н╦н╠о│н╛ н■н╣о┘о└н╜о│н╠ 2023
+
+            DateTime holiday1 = new DateTime(year, 1, 1);
+            DateTime holiday2 = new DateTime(year, 1, 6);
+            DateTime holiday3 = new DateTime(year, 3, 25);
+            DateTime holiday4 = new DateTime(year, 4, 1);
+            DateTime holiday5 = new DateTime(year, 5, 1);
+            DateTime holiday6 = new DateTime(year, 8, 15);
+            DateTime holiday7 = new DateTime(year, 10, 28);
+            DateTime holiday8 = new DateTime(year, 12, 25);
+
+            if (DateTime.Now.Date <= holiday1)
+            {
+                label8.Text = "н═о│о┴о└н©о┤о│н©н╫н╞н╠ " + holiday1.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday2)
+            {
+                label8.Text = "н≤н╣н©о├н╛н╫н╣н╧н╠ " + holiday2.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday3)
+            {
+                label8.Text = "25н╥ н°н╠о│о└н╞н©о┘ " + holiday3.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday4)
+            {
+                label8.Text = "н▒н╫н╣н╬н╠о│о└н╥о┐н╞н╠ о└н╥о┌ н о█о─о│н©о┘ " + holiday4.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday5)
+            {
+                label8.Text = "н∙о│нЁн╠о└н╧н╨н╝ н═о│о┴о└н©н╪н╠о┼н╠ " + holiday5.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday6)
+            {
+                label8.Text = "н н©н╞н╪н╥о┐н╥ о└н╥о┌ н≤н╣н©о└о▄н╨н©о┘ " + holiday6.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday7)
+            {
+                label8.Text = "н≈н╪н╜о│н╠ о└н©о┘ н▄о┤н╧ " + holiday7.ToString("dddd MMMM yyyy");
+            }
+            else if (DateTime.Now.Date <= holiday8)
+            {
+                label8.Text = "н╖о│н╧о┐о└н©о█нЁн╣н╫н╫н╠ " + holiday8.ToString("dddd MMMM yyyy");
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+            dateTimePicker1.ValueChanged += new EventHandler(dateTimePicker1_ValueChanged);
+            AddHolidays(DateTime.Now.Year);
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = DateTime.Now.ToLongDateString();
+            List<int> values = new List<int> { 2, 3, 5, 7, 10 };
+            comboBox1.DataSource = values;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CalculateRemainingDays();
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            CalculateRemainingDays();
+        }
+        private void CalculateRemainingDays()
+        {
+            {
+                if (dateTimePicker1.Value > DateTime.Now)
+                {
+                    label2.Text = "н≈н╪/н╫н╞н╠ н═н╠о│н╠нЁнЁн╣н╩н╞н╠о┌ :";
+                    return;
+                }
+                TimeSpan countworkingdays = DateTime.Now - dateTimePicker1.Value;
+                int workingDays = Enumerable.Range(0, (int)countworkingdays.TotalDays)
+                .Select(i => dateTimePicker1.Value.AddDays(i))
+                .Count(d => (d.DayOfWeek != DayOfWeek.Saturday && d.DayOfWeek != DayOfWeek.Sunday) && !cyprusHolidays.Contains(d.Date));
+                
+                label5.Text = $"{workingDays} н╣о│нЁн╛о┐н╧н╪н╣о┌ н╪н╜о│н╣о┌.";
+            }
 
-        string text1 = "йатастгла келесоу. тевмийо тлгла. о екецвос тоу пяоиомтос окойкгяыхгйе. лпояеите ма пеяасете ма то паяакабете.";
-        string text2 = "йатастгла келесоу. тевмийо тлгла. г еписйеуг тоу пяоиомтос окойкгяыхгйе. лпояеите ма пеяасете ма то паяакабете.";
-        string text3 = "йатастгла келесоу. тевмийо тлгла. г амтийатастасг тоу пяоиомтос окойкгяыхгйе. лпояеите ма пеяасете ма то паяакабете.";
-        string text4 = "йатастгла келесоу. тевмийо тлгла. паяайакы епийоимымгсте алеса лафи лас.";
-        string text5 = "йатастгла келесоу. тевмийо тлгла. то пяоиом сас еимаи пяобкHMAтийо. евете пистысг ─. циа ма паяете йати акко.";
-        string text6 = "йатастгла келесоу. тевмийо тлгла. евете пистысг ─. циа ма паяете йати акко.";
-        string text7 = "йатастгла келесоу. тевмийо тлгла. то пяоиом сас пяоыхеитаи стгм амтипяосыпеиа циа екецво.";
+            // check if the selected year is different from the current year
+            if (dateTimePicker1.Value.Year != DateTime.Now.Year)
+            {
+                // clear the previous year holidays
+                //cyprusHolidays.Clear();
+                // add the new year holidays
+                AddHolidays(dateTimePicker1.Value.Year);
+            }
 
+            //н■н╧н╨н╠н╞о┴н╪н╠ н∙о─н╧о┐о└о│н©о├н╝о┌//
+            TimeSpan doadays = DateTime.Now - dateTimePicker1.Value;
+
+            if (doadays.Days <= 7)
+            {
+                label10.Text = "DOA";
+                label10.ForeColor = Color.Green;
+                label10.Font = new Font(label10.Font, FontStyle.Bold);
+                label11.Text = "Online Return";
+                label11.ForeColor = Color.Green;
+                label11.Font = new Font(label11.Font, FontStyle.Bold);
+            }
+            else if (doadays.Days >= 8 && doadays.Days < 15)
+            {
+                label10.Text = "DOA expired";
+                label10.ForeColor = Color.Red;
+                label10.Font = new Font(label10.Font, FontStyle.Bold);
+                label11.Text = "Online Return";
+                label11.ForeColor = Color.Green;
+                label11.Font = new Font(label11.Font, FontStyle.Bold);
+            }
+            else if (doadays.Days > 15)
+            {
+                label10.Text = "DOA expired";
+                label10.ForeColor = Color.Red;
+                label10.Font = new Font(label10.Font, FontStyle.Bold);
+                label11.Text = "NO Return !!";
+                label11.ForeColor = Color.Red;
+                label11.Font = new Font(label11.Font, FontStyle.Bold);
+            }
+
+            //н≈н╪н╜о│н╣о┌ н∙нЁнЁо█н╥о┐н╥о┌//
+            int minusYears = int.Parse(comboBox1.Text);
+            DateTime pastDate = DateTime.Now.AddYears(-minusYears);
+            DateTime givenDate = dateTimePicker1.Value.Date;
+            TimeSpan difference1 = givenDate - pastDate;
+            int days = (int)difference1.Days;
+
+            if (days >= 0)
+            {
+                label6.Text = "н≈ н╣нЁнЁо█н╥о┐н╥ н╧о┐о┤о█н╣н╧ нЁн╧н╠ н╠н╨о▄н╪н╠ " + days + " н╥н╪н╜о│н╣о┌";
+                label6.ForeColor = Color.Green;
+                label6.Font = new Font(label1.Font, FontStyle.Regular);
+            }
+
+            else
+            {
+                label6.Text = "н∙н╨о└о▄о┌ н╣нЁнЁо█н╥о┐н╥о┌ н╣н╢о▌ н╨н╠н╧ " + days + " н╥н╪н╜о│н╣о┌";
+                label6.ForeColor = Color.Red;
+                label6.Font = new Font(label1.Font, FontStyle.Bold);
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now;
+            label6.ForeColor = Color.Black;
+            dateTimePicker1_ValueChanged(sender, e);
+            comboBox1.SelectedIndex = 0;
+            Clipboard.Clear();
+        }
+        private void button2_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text1);
             Clipboard.SetDataObject(data);
         }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text2);
             Clipboard.SetDataObject(data);
         }
-
-        private void button3_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text3);
             Clipboard.SetDataObject(data);
         }
-        private void button4_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text4);
             Clipboard.SetDataObject(data);
         }
-        private void button5_Click(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text5);
             Clipboard.SetDataObject(data);
         }
-        private void button6_Click(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text6);
             Clipboard.SetDataObject(data);
         }
-        private void button7_Click(object sender, EventArgs e)
+        private void button8_Click(object sender, EventArgs e)
         {
             Clipboard.Clear();
             DataObject data = new DataObject();
             data.SetData(DataFormats.StringFormat, text7);
             Clipboard.SetDataObject(data);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Form form = (Form)((Button)sender).Parent;
+            Button toggleButton = (Button)sender;
+
+            if (toggleButton.Text == "more")
+            {
+                form.Size = new Size(Width = 500, Height = 523);
+                toggleButton.Text = "less";
+            }
+            else
+            {
+                form.Size = new Size(Width = 500, Height = 234);
+                toggleButton.Text = "more";
+            }
         }
     }
 }
